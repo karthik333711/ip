d1407bb361bc9d6cf878897afd5ffe97fca6774c
karthik333711 <139526674+karthik333711@users.noreply.github.com>
2025-01-19 16:16:35 +0530

    Create DJoin.ps1

---
+++ Domain Join.ps1
@@ -0,0 +1,24 @@
+# Prompt the user for the new computer name
+$newComputerName = Read-Host -Prompt "Enter the new computer name"
+
+# Rename the computer
+Rename-Computer -NewName $newComputerName -Force
+
+# Convert the password to a secure string
+$securePassword = ConvertTo-SecureString '*171*&muru343' -AsPlainText -Force
+
+# Create the credential object
+$cred = New-Object System.Management.Automation.PSCredential ('lab\administrator', $securePassword)
+
+# Define parameters for Add-Computer
+$addComputerSplat = @{
+    DomainName = 'lab.com'
+    Credential = $cred
+    Options = 'JoinWithNewName', 'AccountCreate'
+}
+
+# Join the computer to the domain
+Add-Computer @addComputerSplat
+
+# Restart the computer to apply changes
+Restart-Computer
