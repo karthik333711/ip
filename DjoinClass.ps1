efb2069cb80d77cd2e26a99571fa94649dc78f98
karthik333711 <139526674+karthik333711@users.noreply.github.com>
2025-01-20 13:40:26 +0530

    Create DjoinClass.ps1

---
+++ DjoinClass.ps1
@@ -0,0 +1,48 @@
+# Predefined ClassRoom Name
+$ClassRoom = "SCAS-351-"
+
+do {
+    $PcNumber = Read-Host "Enter the PC Number"
+} while (-not $PcNumber)
+
+Write-Host "PC Number Entered: $PcNumber"
+
+# Construct the full computer name
+$newComputerName = "$ClassRoom$PcNumber"
+# Ask for confirmation
+$confirmation = Read-Host "NEW PC NAME IS : $newComputerName"
+
+if ($confirmation -eq "") {
+    Write-Host "Proceeding with the new computer name: $newComputerName"
+} else {
+    Write-Host "Aborted. No changes made."
+}
+
+
+
+
+
+# Rename the computer
+Rename-Computer -NewName $newComputerName -Force
+
+# Convert the password to a secure string
+$securePassword = ConvertTo-SecureString '*171*&muru343' -AsPlainText -Force
+
+# Create the credential object
+$cred = New-Object System.Management.Automation.PSCredential ('lab\administrator', $securePassword)
+
+# Define parameters for Add-Computer
+$addComputerSplat = @{
+    DomainName = 'lab.com'
+    Credential = $cred
+    Options = 'JoinWithNewName', 'AccountCreate'
+}
+
+# Join the computer to the domain
+Add-Computer @addComputerSplat
+
+# Restart the computer to apply changes
+Restart-Computer
+
+
+
