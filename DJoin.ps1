e2f0ce79afaa15e80bd194a91e488af423b1d6cd
karthik333711 <139526674+karthik333711@users.noreply.github.com>
2025-01-20 13:39:46 +0530

    Update DJoin.ps1

--- DJoin.ps1
+++ DJoin.ps1
@@ -1,8 +1,26 @@
-# Prompt the user for the new computer name
-$newComputerName = Read-Host -Prompt "Enter the new computer name"
+do {
+    $newComputerName = Read-Host "Enter the new computer name"
+} while (-not $newComputerName)
+
+Write-Host "PC Number entered: $newComputerName"
+
+
+# Ask for confirmation
+$confirmation = Read-Host "  New computer Name Is : $newComputerName"
+
+if ($confirmation -eq "") {
+    Write-Host "Proceeding with the new computer name: $newComputerName"
+} else {
+    Write-Host "Aborted. No changes made."
+}
+
+
+
+
+
 
 # Rename the computer
-Rename-Computer -NewName SCAS-351-$newComputerName -Force
+Rename-Computer -NewName $newComputerName -Force
 
 # Convert the password to a secure string
 $securePassword = ConvertTo-SecureString '*171*&muru343' -AsPlainText -Force
@@ -22,4 +40,5 @@ Add-Computer @addComputerSplat
 
 # Restart the computer to apply changes
 Restart-Computer
+
 
